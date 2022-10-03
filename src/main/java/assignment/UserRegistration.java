6ade55231615ae1685dd82ce295c6f19a587007d
sunilmsd007 <sunilmsd007@gmail.com>
2022-10-03 23:11:23 +0530

    Added: method to validate email of user

--- src/main/java/assignment/UserRegistration.java
+++ src/main/java/assignment/UserRegistration.java
@@ -19,11 +19,20 @@ public class UserRegistration {
 		return matcher.matches();
 	}
 
+	public boolean validateEmail(String email) {
+		//method to validate email
+		Pattern pattern = Pattern.compile("(abc)[.]?[a-z]*@(bl.co)[.]?[a-z]{0,2}$");
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
 	public static void main(String[] args) {
 		UserRegistration user = new UserRegistration();
 		System.out.println(user.validateFirstName("Sunil")); // valid input
 		System.out.println(user.validateFirstName("sunil")); // invalid input
 		System.out.println(user.validateLastName("Kumar")); // valid input
 		System.out.println(user.validateLastName("kumar")); // invalid input
+		System.out.println(user.validateEmail("abc.xyz@bl.co.in")); // valid input
+		System.out.println(user.validateEmail("abcd.xyz@bl.com.in")); // invalid input
 	}
 }
